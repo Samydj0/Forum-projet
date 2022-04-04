16343d189ccabcbcd1744f7675d49b499e891ef8
Rom1CO <91959201+Rom1CO@users.noreply.github.com>
2022-04-04 10:44:55 +0200

    ajout footer, avancé d ela page index tah les fous

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,9 @@
 * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-business-casual/blob/master/LICENSE)
 */
 // Highlights current date on contact page
+/*
 window.addEventListener('DOMContentLoaded', event => {
     const listHoursArray = document.body.querySelectorAll('.list-hours li');
     listHoursArray[new Date().getDay()].classList.add(('today'));
 })
+*/
